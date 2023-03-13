cfd3b5463a38234d57801ebd0fac51d723d33867
지찬우 <jcw001031@gmail.com>
2023-03-13 15:30:24 +0900

    버튼 클릭 이벤트

---
+++ App1/app/src/main/java/com/woopaca/app1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.woopaca.app1;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(view ->
+                Toast.makeText(getApplicationContext(), "버튼 누름", Toast.LENGTH_SHORT).show());
+
+    }
+}
