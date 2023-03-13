756611b56d5a6300719d44c7224687221ca9ac76
지찬우 <jcw001031@gmail.com>
2023-03-13 15:36:12 +0900

    .gitignore 적용

--- App1/app/src/main/java/com/woopaca/app1/MainActivity.java
+++ App1/app/src/main/java/com/woopaca/app1/MainActivity.java
@@ -1,6 +1,5 @@
 package com.woopaca.app1;
 
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +17,5 @@ public class MainActivity extends AppCompatActivity {
 
         button1.setOnClickListener(view ->
                 Toast.makeText(getApplicationContext(), "버튼 누름", Toast.LENGTH_SHORT).show());
-
     }
 }
