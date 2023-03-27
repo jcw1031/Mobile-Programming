e9c3f5c308176fa35ee97cc1b8f7e9d63b49a39f
지찬우 <jcw001031@gmail.com>
2023-03-27 14:40:22 +0900

    Exercise 5-4

---
+++ Exercise5/app/src/main/java/com/woopaca/exercise5/Exercise54.java
@@ -0,0 +1,13 @@
+package com.woopaca.exercise5;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Exercise54 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercise5_4);
+    }
+}
