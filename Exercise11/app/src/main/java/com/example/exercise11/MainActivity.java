85eec10e8d37ef516a321110950d60de464526b4
jcw1031 <jcw001031@gmail.com>
2023-05-11 09:29:54 +0900

    11주차 연습'

---
+++ Exercise11/app/src/main/java/com/example/exercise11/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.exercise11;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import com.example.exercise11.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);햣
+    }
+}
